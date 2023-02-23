01b537f09721a3008347248340388fc248156633
ztoprakb <ztoprakb@cs.washington.edu>
2023-02-22 23:52:44 -0800

    Make website

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My Homepage</h1>
+      <p>This is my awesome React-based homepage!</p>
+    </div>
+  );
+}
+
+export default HomePage;
+
